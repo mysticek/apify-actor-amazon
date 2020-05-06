5241b017f0ebc012869f33b3aad3921d4d9e3e45
Vladimír Vais <vladimir.vais@bootiq.io>
2020-05-06 08:55:03 +0200

    input schema edit prop

--- src/main.js
+++ src/main.js
@@ -17,9 +17,9 @@ Apify.main(async () => {
   if (!input || !input.sources)
     throw new Error('Input must be a JSON object with the "sources" field!');
 
-  const requestList = await Apify.RequestList({
-    sources: [{ requestFromUrl: input.sources.requestsFromUrl }],
-  });
+  const requestList = await Apify.openRequestList("myUrls", [
+    { requestFromUrl: input.sources.requestsFromUrl },
+  ]);
 
   console.log(requestList);
   return;
