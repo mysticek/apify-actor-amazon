01ea0c01c7f99a17890bc9db12db15e54204a188
Vladimír Vais <vladimir.vais@bootiq.io>
2020-05-20 08:18:18 +0200

    add ENV switch to main.js

--- src/main.js
+++ src/main.js
@@ -4,29 +4,27 @@ const { csvToArray } = require("./helpers");
 const { basicCrawler } = require("./crawlers/basic");
 const { puppeteerCrawler } = require("./crawlers/puppeeter");
 
+// set ENV to production before build to apify
+const ENV = "production";
+
 Apify.main(async () => {
-  // PROD ENVS - uncoment before push to production
+  let DOMAINS_COUNT, DOMAINS_URL, RETRY_COUNT, CRAWLER_TYPE, csv;
 
-  // const input = await Apify.getInput();
-  // const DOMAINS_URL = input.sources[0].requestsFromUrl;
-  // const DOMAINS_COUNT = parseInt(input.domainsCount);
-  // const RETRY_COUNT = 3;
-  // const CRAWLER_TYPE = input.crawlerType;
-
-  // const csv = await request(DOMAINS_URL);
-
-  // ------ end of PROD envs
-
-  // DEV ENVS - comment before push to production
-
-  const DOMAINS_COUNT = 50;
-  const RETRY_COUNT = 3;
-  const CRAWLER_TYPE = "basic";
-  const csv = await request(
-    "https://apify-uploads-prod.s3.amazonaws.com/WoHcDsskx6ERGKznw-XkfjWEqPMiZxDkrZo-2020-04-27_dot_blog_zone_dums_180306_%282%29.csv"
-  );
-
-  // ------ end of DEV envs
+  if (ENV === "production") {
+    const input = await Apify.getInput();
+    DOMAINS_URL = input.sources[0].requestsFromUrl;
+    DOMAINS_COUNT = parseInt(input.domainsCount);
+    RETRY_COUNT = 3;
+    CRAWLER_TYPE = input.crawlerType;
+    csv = await request(DOMAINS_URL);
+  } else {
+    DOMAINS_COUNT = 50;
+    RETRY_COUNT = 3;
+    CRAWLER_TYPE = "basic";
+    csv = await request(
+      "https://apify-uploads-prod.s3.amazonaws.com/WoHcDsskx6ERGKznw-XkfjWEqPMiZxDkrZo-2020-04-27_dot_blog_zone_dums_180306_%282%29.csv"
+    );
+  }
 
   const urls = csvToArray(csv, DOMAINS_COUNT);
   const requestList = await Apify.openRequestList("my-request-list", urls);
